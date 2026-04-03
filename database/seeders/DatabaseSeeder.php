9ffb65fc466d48cf92a66b7a4eb9e4ac4ddd782a
kevins <kevins.choque@gmail.com>
2026-04-02 20:07:40 -0500

    feat(brands): Creacion de 333 registros con factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Client;
+use App\Models\Brand;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,5 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeeder::class);
         // $this->call(ClientSeeder::class);
         Client::factory(66)->create();
+        Brand::factory(333)->create();
     }
 }
